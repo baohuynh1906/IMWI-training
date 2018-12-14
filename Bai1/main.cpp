2f1fbdc73ce087464a8923bf70def2e6710870f3
baohuynh1906 <htbao96@gmail.com>
2018-12-14 14:53:37 +0700

    no message

---
+++ Bai1/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+/*
+int isInteger(char* inStr);
+int isReal(char* inStr);
+int isInRange(double checkNum, double min, double max);
+*/
+
+int main()
+{
+	char chuoi[255];
+	cout << "Nhap chuoi: ";
+	cin >> chuoi;
+
+	system("pause");
+	return 0;
+}
+
+/*
+int isInteger(char* inStr)
+{
+
+}
+
+int isReal(char* inStr)
+{
+
+}
+
+int isInRange(double checkNum, double min, double max)
+{
+
+}
+*/
+/*
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+
+	char kiemtra;
+	cout << "Ban nhap du lieu : " << endl;
+	cin >> kiemtra;
+	if (((kiemtra >= 'A') && (kiemtra <= 'Z')) || ((kiemtra >= 'a') && (kiemtra <= 'z')))
+		cout << "Du lieu ban vua nhap " << kiemtra << " la mot ky tu." << endl;
+	else if ((kiemtra >= '0') && (kiemtra <= '9'))
+		cout << "Du lieu ban vua nhap " << kiemtra << " la mot ky so." << endl;
+	else
+		cout << "Du lieu ban vua nhap " << kiemtra << " la mot bieu tuong." << endl;
+	return 0;
+}
+*/
